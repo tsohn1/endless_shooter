d8e0e911715092b11bd89798779876e01566781f
Teo Sohn <sohnte@gmail.com>
2024-03-04 19:15:55 -0500

    Add playerCamera and Normalize playerSpeed

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Running the Server
+type in `http-server` to start the app. This will start the server on localhost.
--- src/game.js
+++ src/game.js
@@ -19,7 +19,7 @@ const config = {
 const game = new Phaser.Game(config);
 let player;
 var playerRadius = 20;
-var playerSpeed = 200;
+var playerSpeed = 500;
 
 
 function preload() {
@@ -30,6 +30,7 @@ function create() {
   player = this.add.circle(windowWidth/2, windowHeight/2, playerRadius, 0x00ff00);
   player.setOrigin(0.5, 0.5);
 
+  this.cameras.main.startFollow(player);
 
   var graphics = this.add.graphics();
 
@@ -44,6 +45,8 @@ function create() {
 
   // Draw a circle
   graphics.strokeCircle(400, 300, 50);
+  // Draw a circle
+  graphics.strokeCircle(900, 300, 50);
 
   // Draw a line
   graphics.lineBetween(500, 100, 600, 200);
@@ -51,16 +54,32 @@ function create() {
 
 function update() {
   // Handle player movement
+  // Calculate the total velocity based on pressed keys
+  let velocityX = 0;
+  let velocityY = 0;
+
   if (this.input.keyboard.addKey('W').isDown) {
-    player.y -= playerSpeed * this.game.loop.delta / 1000; // Move up
+      velocityY -= 1;
   }
   if (this.input.keyboard.addKey('S').isDown) {
-    player.y += playerSpeed * this.game.loop.delta / 1000; // Move down
+      velocityY += 1;
   }
   if (this.input.keyboard.addKey('A').isDown) {
-    player.x -= playerSpeed * this.game.loop.delta / 1000; // Move left
+      velocityX -= 1;
   }
   if (this.input.keyboard.addKey('D').isDown) {
-    player.x += playerSpeed * this.game.loop.delta / 1000; // Move right
+      velocityX += 1;
   }
+
+  // Normalize the velocity vector
+  const length = Math.sqrt(velocityX ** 2 + velocityY ** 2);
+  if (length !== 0) {
+      velocityX /= length;
+      velocityY /= length;
+  }
+
+  // Set the constant speed
+  const finalSpeed = playerSpeed * this.game.loop.delta / 1000;
+  player.x += velocityX * finalSpeed;
+  player.y += velocityY * finalSpeed;
 }
